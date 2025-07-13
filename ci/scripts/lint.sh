152610e8c73a111de0a546deb0dca27fe77835c6
oluwawunmi <soluwawunmi@gmail.com>
2025-07-13 19:06:22 +0100

    upgrade linter to v2

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 pushd dp-search-data-importer
-  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.5
+  go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.2.1
   make lint
   npm install -g @asyncapi/cli
   make validate-specification
